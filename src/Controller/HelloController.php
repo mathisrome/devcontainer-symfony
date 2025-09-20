bc914a1542e195dce7b92093f7adfd73f7ed37a0
mathisrome <mathis.rome@icloud.com>
2025-09-20 12:31:01 +0200

    test grumphp

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,10 +1,8 @@
 <?php
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-
 final class HelloController extends AbstractController
 {
     #[Route('/hello', name: 'app_hello')]
